f6d67658218834b3b49d8ba986afa9ff099d2263
Ritesh4369 <riteshbhadani4369@gmail.com>
2022-06-10 14:15:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import Home from './Home';
 function App() {
 
